58c42e27dc1a209857f1123674737bd7fe5d945a
mangesh687 <mangeshpakhale2016@gmail.com>
2025-11-29 01:48:26 +0530

    de-stururing

--- src/Components/PersInfo.js
+++ src/Components/PersInfo.js
@@ -1,12 +1,15 @@
 import React from 'react'
-
-function PersInfo(props) {
+function PersInfo({name,age}) {
+//function PersInfo(props) {
+//  const { name, age } = props;
   return (
     <div className='person-card'>
-      <h2>Name:{props.name}</h2>
-      <h2>Age: {props.age}</h2>
+      {/* // <h2>Name:{props.name}</h2>
+      //  <h2>Age: {props.age}</h2> */}
+       <h2>Name:{name}</h2>
+       <h2>Age: {age}</h2>
     </div>
   )
 }
 
-export default PersInfo
+export default PersInfo;
